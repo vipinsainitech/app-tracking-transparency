ec2079dd8a506b423deaa77f607a04fdf8a5c3a0
Vipin Saini <vipinsaini0.me@gmail.com>
2024-05-08 11:13:19 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script>
+import {
+  AppTrackingTransparency,
+  AppTrackingStatusResponse,
+} from 'capacitor-plugin-app-tracking-transparency';
+
+export default {
+  async mounted() {
+    requestPermission();
+// getStatus();
+
+  },
+};
+async function getStatus() {
+  try {
+    const response = await AppTrackingTransparency.getStatus();
+    console.log(response); // { status: 'authorized' } for example
+    status.value = response;
+  } catch (error) {
+    console.error('Error getting status:', error);
+  }
+}
+
+async function requestPermission() {
+  try {
+    const response = await AppTrackingTransparency.requestPermission();
+    console.log(response); // { status: 'authorized' } for example
+    status.value = response;
+  } catch (error) {
+    console.error('Error requesting permission:', error);
+  }
+}
+
+</script>
